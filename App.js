4a75ccb32396a3639bb231b05a38e104d87e6ee0
nooorfatimaa <noorfatima9826@gmail.com>
2020-11-27 23:07:45 +0500

    task 2

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
 
 export default function App() {
+  const[ogPrice, setPrice] = useState("");
+  const[discount, setDiscount] = useState("");
+  const[text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.txt}>Original Price</Text>
+        <TextInput style={styles.txtInput}
+          keyboardType = {"number-pad"}
+        />
+        <Text style={styles.txt}>Discount Percentage</Text>
+        <TextInput style={styles.txtInput}
+          keyboardType = {"number-pad"}
+        />
+      </View>
     </View>
   );
 }
@@ -15,8 +26,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  box: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  txt : {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: '#b395d6', 
+    fontFamily: "monospace",
+  },
+  txtInput: {
+    width: 250,
+    height: 70,
+    borderColor: "coral",
+    borderWidth: 1,
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
